8ddb639141ec7624c060f210fd6ec3b2f6349139
marukoponpon <yui.0707.109@gmail.com>
2026-06-05 04:46:12 +0000

    feat:ポットの性格をいぬに変更

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたはたぬきです。111語尾にぽんぽこをつけて"},
+    {"role": "system", "content": "あなたはいぬです。語尾にわんわんをつけて"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
